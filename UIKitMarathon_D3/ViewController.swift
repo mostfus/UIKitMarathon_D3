9f4e6c07da98e231080e529de83d48eca04c9b91
mostfus <mostfus@yandex-team.ru>
2024-02-10 23:21:47 +0300

    add Vorobey solution

--- UIKitMarathon_D3/ViewController.swift
+++ UIKitMarathon_D3/ViewController.swift
@@ -20,13 +20,15 @@ class ViewController: UIViewController {
 
     private lazy var slider: UISlider = {
         let slider = UISlider(frame: .zero)
-        slider.minimumValue = 0.0
-        slider.maximumValue = 1
+//        slider.minimumValue = 0.0
+//        slider.maximumValue = 1
         slider.tintColor = .systemBlue
 
         return slider
 
     }()
+
+    private var animator = UIViewPropertyAnimator(duration: 0.7, curve: .easeIn)
 
     private var lastSliderValue: Float = 0.0
 
@@ -41,6 +43,7 @@ class ViewController: UIViewController {
 
         slider.addTarget(self, action: #selector(transformSquare(_:)), for: .valueChanged)
         slider.addTarget(self, action: #selector(touchUpInside(_:)), for: .touchUpInside)
+        slider.addTarget(self, action: #selector(touchUpInside(_:)), for: .touchUpOutside)
 
         setupConstraints()
     }
@@ -49,7 +52,7 @@ class ViewController: UIViewController {
         NSLayoutConstraint.activate([
             square.widthAnchor.constraint(equalToConstant: 50),
             square.heightAnchor.constraint(equalToConstant: 50),
-            square.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            square.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor, constant: 30),
             square.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
             slider.topAnchor.constraint(equalTo: square.bottomAnchor, constant: 50),
             slider.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
@@ -57,8 +60,22 @@ class ViewController: UIViewController {
         ])
     }
 
+    override func viewWillLayoutSubviews() {
+        animator.addAnimations {
+            self.square.frame.origin.x = self.view.frame.width - self.view.layoutMargins.right - self.square.frame.width
+
+            self.square.transform = .identity.scaledBy(x: 1.5, y: 1.5).rotated(by: .pi / 2)
+        }
+
+        animator.pausesOnCompletion = true
+    }
+
     @objc
     private func transformSquare(_ sender: UISlider) {
+        animator.fractionComplete = CGFloat(sender.value)
+
+
+        /*
         let minValue = view.layoutMargins.left + square.frame.width / 2
         let maxValue = view.frame.width - view.layoutMargins.right - square.frame.width / 2
         self.square.center.x = minValue + (maxValue - minValue) * CGFloat(sender.value)
@@ -68,15 +85,21 @@ class ViewController: UIViewController {
         square.transform = CGAffineTransform(
             rotationAngle: rotationValue
         ).scaledBy(x: scaleValue, y: scaleValue)
+         */
     }
 
     @objc
     private func touchUpInside(_ sender: UISlider) {
+        animator.startAnimation()
+        sender.setValue(sender.maximumValue, animated: true)
+
+        /*
         sender.setValue(sender.maximumValue, animated: true)
 
         UIView.animate(withDuration: 0.5) {
             self.transformSquare(sender)
         }
+         */
     }
 
 
